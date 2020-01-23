44328ab2c1b837b1a99a1fbf6726881ea5707990
Eli Bishop <eli@launchdarkly.com>
2020-01-23 13:12:07 -0800

    package comments

---
+++ package_info.go
@@ -0,0 +1,7 @@
+// Package ldcore is the base package for Go SDK core types.
+//
+// Currently there are no types in this package; they are all in subpackages such as ldvalue.
+// In a future version, this package will contain the types that are most commonly needed
+// when using the SDK, such as User. The subpackages contain types that applications are
+// less likely to need (and that do not reference anything in the main package), such as Value.
+package ldcore
