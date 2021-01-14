5cf729fc73faf216b598a7ee9bc9e17acc330d76
Eli Bishop <eli@launchdarkly.com>
2021-01-13 16:05:29 -0800

    2.2.2-alpha.1

---
+++ ldvalue/value_serialization_easyjson_test.go
@@ -0,0 +1,65 @@
+// +build launchdarkly_easyjson
+
+package ldvalue
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/mailru/easyjson"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEasyJsonMarshalUnmarshal(t *testing.T) {
+	items := []struct {
+		value Value
+		json  string
+	}{
+		{Null(), nullAsJSON},
+		{Bool(true), "true"},
+		{Bool(false), "false"},
+		{Int(1), "1"},
+		{Float64(1), "1"},
+		{Float64(2.5), "2.5"},
+		{String("x"), `"x"`},
+		{ArrayOf(), `[]`},
+		{ArrayBuild().Add(Bool(true)).Add(String("x")).Build(), `[true,"x"]`},
+		{ObjectBuild().Build(), `{}`},
+		{ObjectBuild().Set("a", Bool(true)).Build(), `{"a":true}`},
+	}
+	for _, item := range items {
+		t.Run(fmt.Sprintf("type %s, json %v", item.value.Type(), item.json), func(t *testing.T) {
+			j, err := easyjson.Marshal(item.value)
+			assert.NoError(t, err)
+			assert.Equal(t, item.json, string(j))
+
+			assert.Equal(t, item.json, item.value.String())
+			assert.Equal(t, item.json, item.value.JSONString())
+			assert.Equal(t, json.RawMessage(item.json), item.value.AsRaw())
+
+			var v Value
+			err = easyjson.Unmarshal([]byte(item.json), &v)
+			assert.NoError(t, err)
+			assert.Equal(t, item.value, v)
+		})
+	}
+}
+
+func TestEasyJsonUnmarshalErrorConditions(t *testing.T) {
+	var v Value
+	for _, data := range [][]byte{
+		nil,
+		[]byte{},
+		[]byte("what"),
+		[]byte("["),
+		[]byte("[what"),
+		[]byte("{"),
+		[]byte("{what"),
+		[]byte(`{"no":what`),
+	} {
+		t.Run(string(data), func(t *testing.T) {
+			assert.Error(t, easyjson.Unmarshal(data, &v))
+		})
+	}
+}
