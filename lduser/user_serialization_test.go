965ab945150bbe7d8fe201688702e0a1464dc59a
Eli Bishop <eli@launchdarkly.com>
2021-01-15 11:24:46 -0800

    EasyJSON optimizations for ldvalue and lduser types (#43)
    * EasyJSON optimizations for ldvalue and lduser types
    
    * revert unnecessary changes to non-easyjson implementation
    
    * fix test (detection of trailing comma is not a well-defined use case)
    
    * require a JSON object (not null) when parsing a User

--- lduser/user_serialization_test.go
+++ lduser/user_serialization_test.go
@@ -7,32 +7,15 @@ import (
 	"gopkg.in/launchdarkly/go-sdk-common.v2/jsonstream"
 	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
 
-	"gopkg.in/launchdarkly/go-jsonstream.v1/jreader"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func getJSONAsMap(t *testing.T, thing interface{}) map[string]interface{} {
-	bytes, err := json.Marshal(thing)
-	require.NoError(t, err)
-	return parseJSONAsMap(t, bytes)
-}
 
 func parseJSONAsMap(t *testing.T, bytes []byte) map[string]interface{} {
 	var props map[string]interface{}
 	err := json.Unmarshal(bytes, &props)
 	require.NoError(t, err)
 	return props
-}
-
-func unmarshalUser(t *testing.T, jsonProps map[string]interface{}) User {
-	bytes, err := json.Marshal(jsonProps)
-	require.NoError(t, err)
-	var user User
-	err = json.Unmarshal(bytes, &user)
-	require.NoError(t, err)
-	return user
 }
 
 func TestUserStringIsJSONRepresentation(t *testing.T) {
@@ -42,62 +25,74 @@ func TestUserStringIsJSONRepresentation(t *testing.T) {
 	assert.Equal(t, parseJSONAsMap(t, bytes), parseJSONAsMap(t, []byte(user.String())))
 }
 
-func TestJSONMarshalProducesCompactRepresentationWhenAttributesAreUnset(t *testing.T) {
-	user := NewUser("some-key")
-	bytes, err := json.Marshal(user)
-	require.NoError(t, err)
-	assert.Equal(t, `{"key":"some-key"}`, string(bytes))
+func TestJSONMarshal(t *testing.T) {
+	doUserMarshalingTests(t, json.Marshal)
 }
 
-func TestJSONMarshalStringAttributes(t *testing.T) {
-	for a, setter := range optionalStringSetters {
-		t.Run(string(a), func(t *testing.T) {
-			builder := NewUserBuilder("some-key")
-			setter(builder, "value")
-			user := builder.Build()
-
-			props := getJSONAsMap(t, user)
-			assert.Equal(t, "some-key", props["key"])
-			assert.Equal(t, "value", props[string(a)])
-
-			for a1, _ := range optionalStringSetters {
-				if a1 != a {
-					unwantedValue, found := props[string(a1)]
-					assert.False(t, found)
-					assert.Nil(t, unwantedValue)
-				}
-			}
-		})
+func doUserMarshalingTests(t *testing.T, marshalFn func(interface{}) ([]byte, error)) {
+	getJSONAsMap := func(t *testing.T, thing interface{}) map[string]interface{} {
+		bytes, err := marshalFn(thing)
+		require.NoError(t, err)
+		return parseJSONAsMap(t, bytes)
 	}
-}
 
-func TestJSONMarshalAnonymousAttribute(t *testing.T) {
-	props1 := getJSONAsMap(t, NewUserBuilder("some-key").Build())
-	assert.Nil(t, props1["anonymous"])
+	t.Run("compact representation with only key", func(t *testing.T) {
+		user := NewUser("some-key")
+		bytes, err := marshalFn(user)
+		require.NoError(t, err)
+		assert.Equal(t, `{"key":"some-key"}`, string(bytes))
+	})
 
-	props2 := getJSONAsMap(t, NewUserBuilder("some-key").Anonymous(true).Build())
-	assert.Equal(t, true, props2["anonymous"])
+	t.Run("string attributes", func(t *testing.T) {
+		for a, setter := range optionalStringSetters {
+			t.Run(string(a), func(t *testing.T) {
+				builder := NewUserBuilder("some-key")
+				setter(builder, "value")
+				user := builder.Build()
 
-	props3 := getJSONAsMap(t, NewUserBuilder("some-key").Anonymous(false).Build())
-	assert.Equal(t, false, props3["anonymous"])
-}
+				props := getJSONAsMap(t, user)
+				assert.Equal(t, "some-key", props["key"])
+				assert.Equal(t, "value", props[string(a)])
 
-func TestJSONMarshalCustomAttribute(t *testing.T) {
-	props1 := getJSONAsMap(t, NewUserBuilder("some-key").Build())
-	assert.Nil(t, props1["custom"])
+				for a1, _ := range optionalStringSetters {
+					if a1 != a {
+						unwantedValue, found := props[string(a1)]
+						assert.False(t, found)
+						assert.Nil(t, unwantedValue)
+					}
+				}
+			})
+		}
+	})
 
-	props2 := getJSONAsMap(t, NewUserBuilder("some-key").Custom("thing", ldvalue.String("value")).Build())
-	assert.Equal(t, map[string]interface{}{"thing": "value"}, props2["custom"])
-}
+	t.Run("anonymous attribute", func(t *testing.T) {
+		props1 := getJSONAsMap(t, NewUserBuilder("some-key").Build())
+		assert.Nil(t, props1["anonymous"])
 
-func TestJSONMarshalPrivateAttributes(t *testing.T) {
-	props1 := getJSONAsMap(t, NewUserBuilder("some-key").Name("value").Build())
-	assert.Equal(t, "value", props1["name"])
-	assert.Nil(t, props1["privateAttributeNames"])
+		props2 := getJSONAsMap(t, NewUserBuilder("some-key").Anonymous(true).Build())
+		assert.Equal(t, true, props2["anonymous"])
 
-	props2 := getJSONAsMap(t, NewUserBuilder("some-key").Name("value").AsPrivateAttribute().Build())
-	assert.Equal(t, "value", props2["name"])
-	assert.Equal(t, []interface{}{"name"}, props2["privateAttributeNames"])
+		props3 := getJSONAsMap(t, NewUserBuilder("some-key").Anonymous(false).Build())
+		assert.Equal(t, false, props3["anonymous"])
+	})
+
+	t.Run("custom attribute", func(t *testing.T) {
+		props1 := getJSONAsMap(t, NewUserBuilder("some-key").Build())
+		assert.Nil(t, props1["custom"])
+
+		props2 := getJSONAsMap(t, NewUserBuilder("some-key").Custom("thing", ldvalue.String("value")).Build())
+		assert.Equal(t, map[string]interface{}{"thing": "value"}, props2["custom"])
+	})
+
+	t.Run("private attributes", func(t *testing.T) {
+		props1 := getJSONAsMap(t, NewUserBuilder("some-key").Name("value").Build())
+		assert.Equal(t, "value", props1["name"])
+		assert.Nil(t, props1["privateAttributeNames"])
+
+		props2 := getJSONAsMap(t, NewUserBuilder("some-key").Name("value").AsPrivateAttribute().Build())
+		assert.Equal(t, "value", props2["name"])
+		assert.Equal(t, []interface{}{"name"}, props2["privateAttributeNames"])
+	})
 }
 
 func TestJSONMarshalWithDeprecatedAPI(t *testing.T) {
@@ -111,137 +106,141 @@ func TestJSONMarshalWithDeprecatedAPI(t *testing.T) {
 	require.JSONEq(t, string(expected), string(actual))
 }
 
-func TestJSONUnmarshalStringAttributes(t *testing.T) {
-	for a, _ := range optionalStringSetters {
-		t.Run(string(a), func(t *testing.T) {
-			props := map[string]interface{}{"key": "some-key"}
-			props[string(a)] = "value"
-			user := unmarshalUser(t, props)
+func TestJSONUnmarshal(t *testing.T) {
+	doUserUnmarshalingTests(t, json.Unmarshal)
+}
 
-			assert.Equal(t, "some-key", user.GetKey())
-			assert.Equal(t, ldvalue.String("value"), user.GetAttribute(a))
-
-			for a1, _ := range optionalStringSetters {
-				if a1 != a {
-					assert.Equal(t, ldvalue.Null(), user.GetAttribute(a1))
-				}
-			}
-		})
+func doUserUnmarshalingTests(t *testing.T, unmarshalFn func([]byte, interface{}) error) {
+	unmarshalUser := func(t *testing.T, jsonProps map[string]interface{}) User {
+		bytes, err := json.Marshal(jsonProps)
+		require.NoError(t, err)
+		var user User
+		err = unmarshalFn(bytes, &user)
+		require.NoError(t, err)
+		return user
 	}
-}
 
-func TestJSONUnmarshalAnonymousAttribute(t *testing.T) {
-	user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key"})
-	assert.Equal(t, ldvalue.Null(), user1.GetAttribute(AnonymousAttribute))
+	t.Run("string attributes", func(t *testing.T) {
+		for a, _ := range optionalStringSetters {
+			t.Run(string(a), func(t *testing.T) {
+				props := map[string]interface{}{"key": "some-key"}
+				props[string(a)] = "value"
+				user := unmarshalUser(t, props)
 
-	user2 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "anonymous": true})
-	assert.Equal(t, ldvalue.Bool(true), user2.GetAttribute(AnonymousAttribute))
+				assert.Equal(t, "some-key", user.GetKey())
+				assert.Equal(t, ldvalue.String("value"), user.GetAttribute(a))
 
-	user3 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "anonymous": false})
-	assert.Equal(t, ldvalue.Bool(false), user3.GetAttribute(AnonymousAttribute))
-}
-
-func TestJSONUnmarshalCustomAttribute(t *testing.T) {
-	user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key"})
-	assert.Equal(t, ldvalue.Null(), user1.GetAttribute(UserAttribute("thing")))
-
-	user2 := unmarshalUser(t, map[string]interface{}{
-		"key":    "some-key",
-		"custom": map[string]interface{}{"thing": "value"},
+				for a1, _ := range optionalStringSetters {
+					if a1 != a {
+						assert.Equal(t, ldvalue.Null(), user.GetAttribute(a1))
+					}
+				}
+			})
+		}
 	})
-	assert.Equal(t, ldvalue.String("value"), user2.GetAttribute(UserAttribute("thing")))
-}
 
-func TestJSONUnmarshalPrivateAttributes(t *testing.T) {
-	user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "name": "value"})
-	assert.False(t, user1.IsPrivateAttribute("name"))
+	t.Run("anonymous attribute", func(t *testing.T) {
+		user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key"})
+		assert.Equal(t, ldvalue.Null(), user1.GetAttribute(AnonymousAttribute))
 
-	user2 := unmarshalUser(t, map[string]interface{}{
-		"key":                   "some-key",
-		"name":                  "value",
-		"privateAttributeNames": []string{"name"},
+		user2 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "anonymous": true})
+		assert.Equal(t, ldvalue.Bool(true), user2.GetAttribute(AnonymousAttribute))
+
+		user3 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "anonymous": false})
+		assert.Equal(t, ldvalue.Bool(false), user3.GetAttribute(AnonymousAttribute))
 	})
-	assert.True(t, user2.IsPrivateAttribute("name"))
-}
 
-func TestJSONUnmarshalNullsAreAllowedForArraysAndObjects(t *testing.T) {
-	user := unmarshalUser(t, map[string]interface{}{"key": "some-key", "custom": nil, "privateAttributeNames": nil})
-	assert.Equal(t, ldvalue.ValueMap{}, user.custom)
-	assert.Nil(t, user.privateAttributes)
-}
+	t.Run("custom attribute", func(t *testing.T) {
+		user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key"})
+		assert.Equal(t, ldvalue.Null(), user1.GetAttribute(UserAttribute("thing")))
 
-func TestJSONUnmarshalDataWithWrongFieldType(t *testing.T) {
-	var user User
-	err := json.Unmarshal([]byte(`{"key":[1,2,3]}`), &user)
-	assert.IsType(t, &json.UnmarshalTypeError{}, err)
-}
+		user2 := unmarshalUser(t, map[string]interface{}{
+			"key":    "some-key",
+			"custom": map[string]interface{}{"thing": "value"},
+		})
+		assert.Equal(t, ldvalue.String("value"), user2.GetAttribute(UserAttribute("thing")))
+	})
 
-func TestJSONUnmarshalNonStructData(t *testing.T) {
-	var user User
-	assert.IsType(t, &json.UnmarshalTypeError{}, json.Unmarshal([]byte(`null`), &user))
-	assert.IsType(t, &json.UnmarshalTypeError{}, json.Unmarshal([]byte(`true`), &user))
-	assert.IsType(t, &json.UnmarshalTypeError{}, json.Unmarshal([]byte(`3`), &user))
-	assert.IsType(t, &json.UnmarshalTypeError{}, json.Unmarshal([]byte(`"x"`), &user))
-	assert.IsType(t, &json.UnmarshalTypeError{}, json.Unmarshal([]byte(`[]`), &user))
-}
+	t.Run("private attributes", func(t *testing.T) {
+		user1 := unmarshalUser(t, map[string]interface{}{"key": "some-key", "name": "value"})
+		assert.False(t, user1.IsPrivateAttribute("name"))
 
-func TestJSONUnmarshalUserKeyValidation(t *testing.T) {
+		user2 := unmarshalUser(t, map[string]interface{}{
+			"key":                   "some-key",
+			"name":                  "value",
+			"privateAttributeNames": []string{"name"},
+		})
+		assert.True(t, user2.IsPrivateAttribute("name"))
+	})
+
+	t.Run("nulls are allowed for arrays and objects", func(t *testing.T) {
+		user := unmarshalUser(t, map[string]interface{}{"key": "some-key", "custom": nil, "privateAttributeNames": nil})
+		assert.Equal(t, ldvalue.ValueMap{}, user.custom)
+		assert.Nil(t, user.privateAttributes)
+	})
+
+	t.Run("data with wrong field type", func(t *testing.T) {
+		var user User
+		err := unmarshalFn([]byte(`{"key":[1,2,3]}`), &user)
+		assert.Error(t, err)
+	})
+
 	t.Run("missing key is invalid", func(t *testing.T) {
 		var user User
-		err := json.Unmarshal([]byte(`{"name":"n"}`), &user)
+		err := unmarshalFn([]byte(`{"name":"n"}`), &user)
 		assert.Equal(t, ErrMissingKey(), err)
 	})
 
 	t.Run("null key is invalid", func(t *testing.T) {
 		var user User
-		err := json.Unmarshal([]byte(`{"key":null,"name":"n"}`), &user)
+		err := unmarshalFn([]byte(`{"key":null,"name":"n"}`), &user)
 		assert.Equal(t, ErrMissingKey(), err)
 	})
 
 	t.Run("empty string key is valid", func(t *testing.T) {
 		var user User
-		err := json.Unmarshal([]byte(`{"key":"","name":"n"}`), &user)
+		err := unmarshalFn([]byte(`{"key":"","name":"n"}`), &user)
 		assert.NoError(t, err)
 		assert.Equal(t, "", user.GetKey())
 		assert.Equal(t, "n", user.GetName().StringValue())
+	})
+
+	t.Run("unknown attributes are ignored", func(t *testing.T) {
+		user := unmarshalUser(t, map[string]interface{}{
+			"key":  "some-key",
+			"a":    3,
+			"b":    nil,
+			"c":    []interface{}{},
+			"name": "value",
+		})
+		assert.Equal(t, "some-key", user.GetKey())
+		assert.Equal(t, "value", user.GetName().StringValue())
+	})
+
+	t.Run("malformed data", func(t *testing.T) {
+		var user User
+		for _, data := range [][]byte{
+			nil,
+			[]byte{},
+			[]byte("null"),
+			[]byte("true"),
+			[]byte("[]"),
+			[]byte("{"),
+			[]byte("{true"),
+			[]byte(`{"key":true}`),
+			[]byte(`{"name":true}`),
+			[]byte(`{"privateAttributeNames":{}`),
+			[]byte(`{"privateAttributeNames":[true]`),
+		} {
+			t.Run(string(data), func(t *testing.T) {
+				err := unmarshalFn(data, &user)
+				assert.Error(t, err)
+				assert.NotEqual(t, ErrMissingKey(), err)
+			})
+		}
 	})
 }
 
 func TestMissingKeyHasErrorMessage(t *testing.T) {
 	assert.NotEqual(t, "", ErrMissingKey().Error())
-}
-
-func TestJSONUnmarshalUnknownAttributesAreIgnored(t *testing.T) {
-	user := unmarshalUser(t, map[string]interface{}{
-		"key":  "some-key",
-		"a":    3,
-		"b":    nil,
-		"c":    []interface{}{},
-		"name": "value",
-	})
-	assert.Equal(t, "some-key", user.GetKey())
-	assert.Equal(t, "value", user.GetName().StringValue())
-}
-
-func TestJSONUnmarshalMalformedData(t *testing.T) {
-	var user User
-	for _, data := range [][]byte{
-		nil,
-		[]byte{},
-		[]byte("true"),
-		[]byte("[]"),
-		[]byte("{"),
-		[]byte("{true"),
-		[]byte(`{"key":true}`),
-		[]byte(`{"name":true}`),
-		[]byte(`{"privateAttributeNames":{}`),
-		[]byte(`{"privateAttributeNames":[true]`),
-	} {
-		t.Run(string(data), func(t *testing.T) {
-			assert.Error(t, json.Unmarshal(data, &user))
-			r := jreader.NewReader(data)
-			user.ReadFromJSONReader(&r)
-			assert.Error(t, r.Error())
-		})
-	}
 }
--- ldvalue/value_serialization_easyjson.go
+++ ldvalue/value_serialization_easyjson.go
@@ -3,9 +3,6 @@
 package ldvalue
 
 import (
-	"gopkg.in/launchdarkly/go-jsonstream.v1/jreader"
-	"gopkg.in/launchdarkly/go-jsonstream.v1/jwriter"
-
 	"github.com/mailru/easyjson/jlexer"
 	ej_jwriter "github.com/mailru/easyjson/jwriter"
 )
@@ -17,73 +14,163 @@ import (
 // encoding/json, and will call them if present. But this mechanism is inefficient: when marshaling
 // it requires the allocation of intermediate byte slices, and when unmarshaling it causes the
 // JSON object to be parsed twice. It is preferable to have our marshal/unmarshal methods write to
-// and read from the EasyJSON Writer/Lexer directly. Our go-jsonstream library provides methods for
-// doing this, if the launchdarkly_easyjson build tag is set.package ldmodel
-//
+// and read from the EasyJSON Writer/Lexer directly. Also, since deserialization is a high-traffic
+// path in some LaunchDarkly code on the service side, the extra overhead of the go-jsonstream
+// abstraction is undesirable.
+
 // For more information, see: https://gopkg.in/launchdarkly/go-jsonstream.v1
 
 func (v Value) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	switch v.valueType {
+	case NullType:
+		writer.Raw(nullAsJSONBytes, nil)
+	case BoolType:
+		writer.Bool(v.boolValue)
+	case NumberType:
+		writer.Float64(v.numberValue)
+	case StringType:
+		writer.String(v.stringValue)
+	case ArrayType:
+		v.arrayValue.MarshalEasyJSON(writer)
+	case ObjectType:
+		v.objectValue.MarshalEasyJSON(writer)
+	}
 }
 
 func (v *Value) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsDelim('[') {
+		var va ValueArray
+		va.UnmarshalEasyJSON(lexer)
+		*v = Value{valueType: ArrayType, arrayValue: va}
+	} else if lexer.IsDelim('{') {
+		var vm ValueMap
+		vm.UnmarshalEasyJSON(lexer)
+		*v = Value{valueType: ObjectType, objectValue: vm}
+	} else {
+		*v = CopyArbitraryValue(lexer.Interface())
+	}
 }
 
 func (v OptionalBool) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	if v.hasValue {
+		writer.Bool(v.value)
+	} else {
+		writer.Raw(nullAsJSONBytes, nil)
+	}
 }
 
 func (v *OptionalBool) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Null()
+		*v = OptionalBool{}
+		return
+	}
+	v.hasValue = true
+	v.value = lexer.Bool()
 }
 
 func (v OptionalInt) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	if v.hasValue {
+		writer.Int(v.value)
+	} else {
+		writer.Raw(nullAsJSONBytes, nil)
+	}
 }
 
 func (v *OptionalInt) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Null()
+		*v = OptionalInt{}
+		return
+	}
+	v.hasValue = true
+	v.value = lexer.Int()
 }
 
 func (v OptionalString) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	if v.hasValue {
+		writer.String(v.value)
+	} else {
+		writer.Raw(nullAsJSONBytes, nil)
+	}
 }
 
 func (v *OptionalString) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Null()
+		*v = OptionalString{}
+		return
+	}
+	v.hasValue = true
+	v.value = lexer.String()
 }
 
 func (v ValueArray) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	if v.data == nil {
+		writer.Raw(nullAsJSONBytes, nil)
+		return
+	}
+	writer.RawByte('[')
+	for i, value := range v.data {
+		if i != 0 {
+			writer.RawByte(',')
+		}
+		value.MarshalEasyJSON(writer)
+	}
+	writer.RawByte(']')
 }
 
 func (v *ValueArray) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Null()
+		*v = ValueArray{}
+		return
+	}
+	lexer.Delim('[')
+	v.data = make([]Value, 0, 4)
+	for !lexer.IsDelim(']') {
+		var value Value
+		value.UnmarshalEasyJSON(lexer)
+		v.data = append(v.data, value)
+		lexer.WantComma()
+	}
+	lexer.Delim(']')
 }
 
 func (v ValueMap) MarshalEasyJSON(writer *ej_jwriter.Writer) {
-	wrappedWriter := jwriter.NewWriterFromEasyJSONWriter(writer)
-	v.WriteToJSONWriter(&wrappedWriter)
+	if v.data == nil {
+		writer.Raw(nullAsJSONBytes, nil)
+		return
+	}
+	writer.RawByte('{')
+	first := true
+	for key, value := range v.data {
+		if !first {
+			writer.RawByte(',')
+		}
+		first = false
+		writer.String(key)
+		writer.RawByte(':')
+		value.MarshalEasyJSON(writer)
+	}
+	writer.RawByte('}')
 }
 
 func (v *ValueMap) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	v.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Null()
+		*v = ValueMap{}
+		return
+	}
+	v.data = make(map[string]Value)
+	lexer.Delim('{')
+	for !lexer.IsDelim('}') {
+		key := string(lexer.String())
+		lexer.WantColon()
+		var value Value
+		value.UnmarshalEasyJSON(lexer)
+		v.data[key] = value
+		lexer.WantComma()
+	}
+	lexer.Delim('}')
 }
