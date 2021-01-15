f0167171f62861606a81a237e78f44ec8b5d404c
LaunchDarklyCI <LaunchDarklyCI@users.noreply.github.com>
2021-01-15 19:42:43 +0000

    prepare 2.2.2 release (#16)

--- lduser/user_serialization_easyjson.go
+++ lduser/user_serialization_easyjson.go
@@ -3,8 +3,8 @@
 package lduser
 
 import (
-	"gopkg.in/launchdarkly/go-jsonstream.v1/jreader"
 	"gopkg.in/launchdarkly/go-jsonstream.v1/jwriter"
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
 
 	"github.com/mailru/easyjson/jlexer"
 	ej_jwriter "github.com/mailru/easyjson/jwriter"
@@ -17,8 +17,10 @@ import (
 // encoding/json, and will call them if present. But this mechanism is inefficient: when marshaling
 // it requires the allocation of intermediate byte slices, and when unmarshaling it causes the
 // JSON object to be parsed twice. It is preferable to have our marshal/unmarshal methods write to
-// and read from the EasyJSON Writer/Lexer directly. Our go-jsonstream library provides methods for
-// doing this, if the launchdarkly_easyjson build tag is set.package ldmodel
+// and read from the EasyJSON Writer/Lexer directly. Also, since user deserialization is a
+// high-traffic path in some LaunchDarkly code on the service side, the extra overhead of the
+// go-jsonstream abstraction is undesirable and we'll instead use an EasyJSON-generated
+// deserializer for an intermediate struct type.
 //
 // For more information, see: https://gopkg.in/launchdarkly/go-jsonstream.v1
 
@@ -28,7 +30,52 @@ func (u User) MarshalEasyJSON(writer *ej_jwriter.Writer) {
 }
 
 func (u *User) UnmarshalEasyJSON(lexer *jlexer.Lexer) {
-	wrappedReader := jreader.NewReaderFromEasyJSONLexer(lexer)
-	u.ReadFromJSONReader(&wrappedReader)
-	lexer.AddError(wrappedReader.Error())
+	if lexer.IsNull() {
+		lexer.Delim('{') // to trigger an "expected an object, got null" error
+		return
+	}
+	var fields userEquivalentStruct
+	fields.UnmarshalEasyJSON(lexer)
+	if lexer.Error() != nil {
+		return
+	}
+	if !fields.Key.IsDefined() {
+		lexer.AddError(ErrMissingKey())
+		return
+	}
+	u.key = fields.Key.StringValue()
+	u.secondary = fields.Secondary
+	u.ip = fields.Ip
+	u.country = fields.Country
+	u.email = fields.Email
+	u.firstName = fields.FirstName
+	u.lastName = fields.LastName
+	u.avatar = fields.Avatar
+	u.name = fields.Name
+	u.anonymous = fields.Anonymous
+	u.custom = fields.Custom
+	if len(fields.PrivateAttributeNames) != 0 {
+		u.privateAttributes = make(map[UserAttribute]struct{}, len(fields.PrivateAttributeNames))
+		for _, a := range fields.PrivateAttributeNames {
+			u.privateAttributes[UserAttribute(a)] = struct{}{}
+		}
+	}
+}
+
+//go:generate easyjson -build_tags launchdarkly_easyjson -output_filename user_serialization_easyjson_generated.go user_serialization_easyjson.go
+
+//easyjson:json
+type userEquivalentStruct struct {
+	Key                   ldvalue.OptionalString `json:"key"`
+	Secondary             ldvalue.OptionalString `json:"secondary,omitempty"`
+	Ip                    ldvalue.OptionalString `json:"ip,omitempty"`
+	Country               ldvalue.OptionalString `json:"country,omitempty"`
+	Email                 ldvalue.OptionalString `json:"email,omitempty"`
+	FirstName             ldvalue.OptionalString `json:"firstName,omitempty"`
+	LastName              ldvalue.OptionalString `json:"lastName,omitempty"`
+	Avatar                ldvalue.OptionalString `json:"avatar,omitempty"`
+	Name                  ldvalue.OptionalString `json:"name,omitempty"`
+	Anonymous             ldvalue.OptionalBool   `json:"anonymous,omitempty"`
+	Custom                ldvalue.ValueMap       `json:"custom,omitempty"`
+	PrivateAttributeNames []string               `json:"privateAttributeNames,omitempty"`
 }
