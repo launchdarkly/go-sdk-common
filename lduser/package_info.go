c00bea30dd74aacbdb4c80779a34b1d693fe724c
Eli Bishop <eli@launchdarkly.com>
2022-02-28 10:13:04 -0800

    (#6) reimplement UserBuilder to build Context (#64)

--- lduser/package_info.go
+++ lduser/package_info.go
@@ -1,2 +1,113 @@
-// Package lduser defines the LaunchDarkly SDK model for user properties.
+// Package lduser defines the older LaunchDarkly SDK model for user properties.
+//
+// The SDK now uses the type ldcontext.Context to represent an evaluation context that might
+// represent a user, or some other kind of entity, or multiple kinds. But in older SDK versions,
+// this was limited to one kind and was represented by the type lduser.User. This differed from
+// ldcontext.Context in several ways:
+//
+// - There was always a single implicit context kind of "user".
+//
+// - Unlike Context where only a few attributes such as Key and Name have special behavior, the
+// user model defined many other built-in attributes such as Email which, like Name, were constrained
+// to only allow string values. These had specific setter methods in UserBuilder.
+//
+// - The property that is now called Transient was called Anonymous.
+//
+// Updating code while still using UserBuilder
+//
+// The lduser.User type has been removed; the SDK now operates only on Contexts. However,
+// the lduser.UserBuilder type has been retained and modified to be a wrapper for ldcontext.Builder.
+// This allows code that used the older model to still work with minor adjustments.
+//
+// For any code that still uses UserBuilder, the significant differences from older SDK versions are:
+//
+// 1. The Build() method now returns an ldcontext.Context, so you will need to update any part of
+// your code that referred to the lduser.User type by name.
+//
+// 2. The SDK no longer supports setting the key to an empty string. If you do this, the returned
+// Context will be invalid (as indicated by its Err() method returning an error) and the SDK will
+// refuse to use it for evaluations or events.
+//
+// 3. Previously, the Anonymous property (now called Transient) had three states: true, false, or
+// undefined/null. Undefined/null and false were functionally the same in terms of the LaunchDarkly
+// dashboard/indexing behavior, but they were represented differently in JSON and could behave
+// differently if referenced in a flag rule (an undefined/null value would not match "anonymous is
+// false"). Now, the property is a simple boolean defaulting to false, and the undefined state is
+// the same as false.
+//
+// Migrating from UserBuilder to the ldcontext API
+//
+// It is preferable to update existing code to use the ldcontext package directly, rather than
+// the UserBuilder wrapper. Here are the kinds of changes you may need to make:
+//
+// - Code that previously created a simple User with only a key should now use ldcontext.New().
+//
+//     // old
+//     user := lduser.NewUser("my-user-key")
+//
+//     // new
+//     user := ldcontext.New("my-user-key")
+//
+// - Code that previously created a User with an empty string key ("") must be changed to use a
+// non-empty key instead. If you do not care about the value of the key, use an arbitrary value.
+// If you do not want the key to appear on your LaunchDarkly dashboard, use Transient.
+//
+// - Code that previously used UserBuilder should now use ldcontext.NewBuilder().
+//
+// - The ldcontext Builder has fewer attribute-name-specific setter methods: Name is still a
+// built-in attribute with its own setter, but for all other optional attributes such as Email
+// that you are setting to a string value, you should instead call Builder.SetString() and
+// specify the attribute name as the first parameter.
+//
+//     // old
+//     user := lduser.NewUserBuilder("my-user-key").
+//         Name("my-name").
+//         Email("my-email").
+//         Build()
+//
+//     // new
+//     user := ldcontext.NewBuilder("my-user-key").
+//         Name("my-name").
+//         SetString("email", "my-email").
+//         Build()
+//
+// - The SetCustom method has been replaced by several Set methods for specific value types,
+// and the SetValue method which takes an ldvalue.Value representing a value of any type
+// (boolean, number, string, array, or object).
+//
+//     // old
+//     user := lduser.NewUserBuilder("my-user-key").
+//         Custom("my-string-attr", ldvalue.String("value")).
+//         Custom("my-array-attr", ldvalue.ArrayOf(ldvalue.String("a"), ldvalue.String("b"))).
+//         Build()
+//
+//     // new
+//     user := ldcontext.NewBuilder("my-user-key").
+//         SetString("my-string-attr", "value").
+//         SetValue("my-array-attr", ldvalue.ArrayOf(ldvalue.String("a"), ldvalue.String("b"))).
+//         Build()
+//
+// - Anonymous has been renamed to Transient.
+//
+//     // old
+//     user := lduser.NewUserBuilder("my-user-key").Anonymous(true).Build()
+//
+//     // new
+//     user := ldcontext.NewBuilder("my-user-key").Transient(true).Build()
+//
+// - Private attributes are now designated by attribute name, instead of by chaining a call to
+// AsPrivateAttribute() after calling the setter.
+//
+//     // old
+//     user := lduser.NewUserBuilder("my-user-key").
+//         Name("my-name").AsPrivateAttribute().
+//         Email("my-email").AsPrivateAttribute().
+//         Build()
+//
+//     // new
+//     user := ldcontext.NewBuilder("my-user-key").
+//         Name("my-name").
+//         SetString("email", "my-email").
+//         Private("name", "email").
+//         Build()
 package lduser
--- lduser/user_test.go
+++ lduser/user_test.go
@@ -1,13 +1,13 @@
 package lduser
 
 import (
-	"fmt"
 	"sort"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldcontext"
 	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var allBuiltInAttributes = []UserAttribute{
@@ -23,15 +23,22 @@ var allBuiltInAttributes = []UserAttribute{
 	AnonymousAttribute,
 }
 
-var optionalStringGetters = map[UserAttribute]func(User) ldvalue.OptionalString{
-	SecondaryKeyAttribute: User.GetSecondaryKey,
-	IPAttribute:           User.GetIP,
-	CountryAttribute:      User.GetCountry,
-	EmailAttribute:        User.GetEmail,
-	FirstNameAttribute:    User.GetFirstName,
-	LastNameAttribute:     User.GetLastName,
-	AvatarAttribute:       User.GetAvatar,
-	NameAttribute:         User.GetName,
+func makeStringGetter(name string) func(c ldcontext.Context) ldvalue.OptionalString {
+	return func(c ldcontext.Context) ldvalue.OptionalString {
+		v, _ := c.GetValue(name)
+		return v.AsOptionalString()
+	}
+}
+
+var optionalStringGetters = map[UserAttribute]func(ldcontext.Context) ldvalue.OptionalString{
+	SecondaryKeyAttribute: makeStringGetter("secondary"),
+	IPAttribute:           makeStringGetter("ip"),
+	CountryAttribute:      makeStringGetter("country"),
+	EmailAttribute:        makeStringGetter("email"),
+	FirstNameAttribute:    makeStringGetter("firstName"),
+	LastNameAttribute:     makeStringGetter("lastName"),
+	AvatarAttribute:       makeStringGetter("avatar"),
+	NameAttribute:         makeStringGetter("name"),
 }
 
 var optionalStringSetters = map[UserAttribute]func(UserBuilder, string) UserBuilderCanMakeAttributePrivate{
@@ -45,169 +52,21 @@ var optionalStringSetters = map[UserAttribute]func(UserBuilder, string) UserBuil
 	NameAttribute:         UserBuilder.Name,
 }
 
-func assertStringAttrNotSet(t *testing.T, a UserAttribute, user User) {
-	assert.Equal(t, ldvalue.OptionalString{}, optionalStringGetters[a](user), "should not have had a value for %s", a)
-	assert.Equal(t, ldvalue.Null(), user.GetAttribute(a), "should not have had a value for %s", a)
+func assertStringAttrNotSet(t *testing.T, a UserAttribute, c ldcontext.Context) {
+	assert.Equal(t, ldvalue.OptionalString{}, optionalStringGetters[a](c), "should not have had a value for %s", a)
 }
 
-func assertStringPropertiesNotSet(t *testing.T, user User) {
-	for a, _ := range optionalStringGetters {
-		assertStringAttrNotSet(t, a, user)
-	}
-}
-
-func getCustomAttrs(user User) []string {
-	var ret []string
-	user.GetAllCustom().Enumerate(func(i int, a string, v ldvalue.Value) bool {
-		ret = append(ret, a)
-		return true
-	})
+func sortedOptionalAttributes(c ldcontext.Context) []string {
+	ret := c.GetOptionalAttributeNames(nil)
 	sort.Strings(ret)
 	return ret
 }
 
-func getPrivateAttrs(user User) []string {
-	var ret []string
-	for _, a := range allBuiltInAttributes {
-		if user.IsPrivateAttribute(a) {
-			ret = append(ret, string(a))
-		}
+func sortedPrivateAttributes(c ldcontext.Context) []string {
+	ret := make([]string, 0, c.PrivateAttributeCount())
+	for i := 0; i < c.PrivateAttributeCount(); i++ {
+		a, _ := c.PrivateAttributeByIndex(i)
+		ret = append(ret, a.String())
 	}
-	user.GetAllCustom().Enumerate(func(i int, a string, v ldvalue.Value) bool {
-		if user.IsPrivateAttribute(UserAttribute(a)) {
-			ret = append(ret, a)
-		}
-		return true
-	})
-	sort.Strings(ret)
 	return ret
-}
-
-func TestNewUser(t *testing.T) {
-	user := NewUser("some-key")
-
-	assert.Equal(t, "some-key", user.GetKey())
-	assert.Equal(t, ldvalue.String("some-key"), user.GetAttribute(KeyAttribute))
-
-	assertStringPropertiesNotSet(t, user)
-
-	anon, found := user.GetAnonymousOptional()
-	assert.False(t, anon)
-	assert.False(t, found)
-	assert.Equal(t, ldvalue.Null(), user.GetAttribute(AnonymousAttribute))
-
-	assert.Nil(t, getCustomAttrs(user))
-	assert.Nil(t, getPrivateAttrs(user))
-	assert.False(t, user.HasPrivateAttributes())
-}
-
-func TestNewAnonymousUser(t *testing.T) {
-	user := NewAnonymousUser("some-key")
-
-	assert.Equal(t, "some-key", user.GetKey())
-	assert.Equal(t, ldvalue.String("some-key"), user.GetAttribute(KeyAttribute))
-
-	assertStringPropertiesNotSet(t, user)
-
-	anon, found := user.GetAnonymousOptional()
-	assert.True(t, anon)
-	assert.True(t, found)
-	assert.Equal(t, ldvalue.Bool(true), user.GetAttribute(AnonymousAttribute))
-
-	assert.Nil(t, getCustomAttrs(user))
-	assert.Nil(t, getPrivateAttrs(user))
-	assert.False(t, user.HasPrivateAttributes())
-}
-
-func TestGetUnknownAttribute(t *testing.T) {
-	assert.Equal(t, ldvalue.Null(), NewUser("some-key").GetAttribute(UserAttribute("no-such-thing")))
-}
-
-func TestEnumerateCustomAttributes(t *testing.T) {
-	user := NewUserBuilder("some-key").Custom("first", ldvalue.Int(1)).Custom("second", ldvalue.String("two")).Build()
-	m := make(map[string]ldvalue.Value)
-	user.GetAllCustom().Enumerate(func(i int, a string, v ldvalue.Value) bool {
-		m[a] = v
-		return true
-	})
-	assert.Equal(t, map[string]ldvalue.Value{"first": ldvalue.Int(1), "second": ldvalue.String("two")}, m)
-}
-
-func TestUserWithNoCustomAttributes(t *testing.T) {
-	user := NewUser("some-key")
-
-	assert.Nil(t, getCustomAttrs(user))
-
-	value, ok := user.GetCustom("attr")
-	assert.False(t, ok)
-	assert.Equal(t, ldvalue.Null(), value)
-
-	assert.Nil(t, getCustomAttrs(user))
-}
-
-func TestUserEqualsComparesAllAttributes(t *testing.T) {
-	shouldEqual := func(a User, b User) {
-		assert.True(t, b.Equal(a), "%s should equal %s", b, a)
-		assert.True(t, a.Equal(b), "%s should equal %s", a, b)
-	}
-	shouldNotEqual := func(a User, b User) {
-		assert.False(t, b.Equal(a), "%s should not equal %s", b, a)
-		assert.False(t, a.Equal(b), "%s should not equal %s", a, b)
-	}
-
-	user0 := NewUser("some-key")
-	assert.True(t, user0.Equal(user0), "%s should equal itself", user0)
-
-	user1 := newUserBuilderWithAllPropertiesSet("some-key").Build()
-	assert.True(t, user1.Equal(user1), "%s should equal itself", user1)
-	user2 := NewUserBuilderFromUser(user1).Build()
-	shouldEqual(user1, user2)
-
-	for a, setter := range optionalStringSetters {
-		builder3 := NewUserBuilderFromUser(user1)
-		setter(builder3, "different-value")
-		user3 := builder3.Build()
-		shouldNotEqual(user1, user3)
-
-		builder4 := NewUserBuilderFromUser(user1)
-		setter(builder4, fmt.Sprintf("value-%s", a)).AsPrivateAttribute()
-		user4 := builder4.Build()
-		shouldNotEqual(user1, user4)
-	}
-
-	shouldNotEqual(user1, NewUserBuilderFromUser(user1).Key("other-key").Build())
-
-	shouldNotEqual(user0, NewUserBuilderFromUser(user0).Anonymous(true).Build())
-	shouldNotEqual(NewUserBuilderFromUser(user0).Anonymous(true).Build(), NewUserBuilderFromUser(user0).Anonymous(false).Build())
-
-	// modifying an existing custom attribute
-	shouldNotEqual(user1, NewUserBuilderFromUser(user1).Custom("thing1", ldvalue.String("other-value")).Build())
-
-	// adding a new custom attribute
-	shouldNotEqual(user1, NewUserBuilderFromUser(user1).Custom("thing3", ldvalue.String("other-value")).Build())
-
-	// adding an extra private attribute
-	shouldNotEqual(user1, NewUserBuilderFromUser(user1).Custom("thing1", ldvalue.String("value1")).AsPrivateAttribute().Build())
-
-	// having the same number of private attributes, but not the same ones
-	shouldNotEqual(
-		NewUserBuilderFromUser(user1).
-			Custom("thing3", ldvalue.Bool(true)).AsPrivateAttribute().
-			Custom("thing4", ldvalue.Bool(true)).
-			Build(),
-		NewUserBuilderFromUser(user1).
-			Custom("thing3", ldvalue.Bool(true)).
-			Custom("thing4", ldvalue.Bool(true)).AsPrivateAttribute().
-			Build())
-}
-
-func newUserBuilderWithAllPropertiesSet(key string) UserBuilder {
-	builder := NewUserBuilder(key)
-	for a, setter := range optionalStringSetters {
-		setter(builder, fmt.Sprintf("value-%s", a))
-	}
-	builder.Anonymous(true)
-	builder.Custom("thing1", ldvalue.String("value1"))
-	builder.Custom("thing2", ldvalue.String("value2")).AsPrivateAttribute()
-	return builder
 }
