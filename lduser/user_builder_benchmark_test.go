91fe3b936c4943f362874428d182ebfd4e43ae0d
Eli Bishop <eli@launchdarkly.com>
2022-03-01 09:55:06 -0800

    add comparative benchmarks for v2 (#66)
    * add comparative benchmarks for v2
    
    * DRY

--- lduser/user_builder_benchmark_test.go
+++ lduser/user_builder_benchmark_test.go
@@ -1,25 +1,10 @@
 package lduser
 
 import (
+	"fmt"
 	"testing"
 
-	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
-)
-
-var (
-	benchmarkUserBuilder                        = NewUserBuilder("key")
-	benchmarkUserBuilderWithAllScalarAttributes = NewUserBuilder("key").
-							Secondary("s").
-							IP("i").
-							Country("c").
-							Email("e").
-							FirstName("f").
-							LastName("l").
-							Avatar("a").
-							Name("n").
-							Anonymous(true)
-
-	benchmarkUserResult User
+	"gopkg.in/launchdarkly/go-sdk-common.v2/internal/sharedtest"
 )
 
 func BenchmarkNewUserNoAlloc(b *testing.B) {
@@ -40,12 +25,19 @@ func BenchmarkNewUserFromBuilder(b *testing.B) {
 	}
 }
 
-func BenchmarkNewUserFromBuilderWithCustom(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchmarkUserResult = NewUserBuilder("key").
-			Custom("attr1", ldvalue.String("value1")).
-			Custom("attr2", ldvalue.String("value2")).
-			Build()
+func BenchmarkBuildWithCustomAttributes(b *testing.B) {
+	for _, n := range []int{sharedtest.SmallNumberOfCustomAttributes, sharedtest.LargeNumberOfCustomAttributes} {
+		b.Run(fmt.Sprintf("with %d attributes", n), func(b *testing.B) {
+			attrs := sharedtest.MakeCustomAttributeNamesAndValues(n)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				builder := NewUserBuilder("key")
+				for _, a := range attrs {
+					builder.SetAttribute(UserAttribute(a.Name), a.Value)
+				}
+				benchmarkUserResult = builder.Build()
+			}
+		})
 	}
 }
 
