de00407e0aef3365ec0f6719ffe3ab055780d6b6
Eli Bishop <eli@launchdarkly.com>
2022-06-29 13:24:14 -0700

    transparently convert empty raw JSON value to null

--- ldvalue/value_serialization.go
+++ ldvalue/value_serialization.go
@@ -77,7 +77,11 @@ func (v Value) MarshalJSON() ([]byte, error) {
 	case ObjectType:
 		return v.objectValue.MarshalJSON()
 	case RawType:
-		return []byte(v.stringValue), nil
+		if len(v.stringValue) == 0 {
+			return nullAsJSONBytes, nil // we don't check for other kinds of malformed JSON here, but if it was nil/"" we can assume they meant null
+		} else {
+			return []byte(v.stringValue), nil
+		}
 	}
 	return nil, errors.New("unknown data type") // should not be possible
 }
@@ -137,7 +141,11 @@ func (v Value) WriteToJSONWriter(w *jwriter.Writer) {
 	case ObjectType:
 		v.objectValue.WriteToJSONWriter(w)
 	case RawType:
-		w.Raw([]byte(v.stringValue))
+		if len(v.stringValue) == 0 {
+			w.Null() // we don't check for other kinds of malformed JSON here, but if it was nil/"" we can assume they meant null
+		} else {
+			w.Raw([]byte(v.stringValue))
+		}
 	}
 }
 
